780c349694667d0c3c87e04dc1ed2b0c86dc11e4
nimaytenzin <nimaytenzin@gmail.com>
2024-06-27 15:49:33 +0600

    activated service worker and auto update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 import { PrimeNGConfig } from 'primeng/api';
+import { from } from 'rxjs';
 
 @Component({
     selector: 'app-root',
-    templateUrl: './app.component.html'
+    templateUrl: './app.component.html',
 })
 export class AppComponent implements OnInit {
-
-    constructor(private primengConfig: PrimeNGConfig) { }
+    constructor(
+        private primengConfig: PrimeNGConfig,
+        private swUpdate: SwUpdate
+    ) {}
 
     ngOnInit() {
         this.primengConfig.ripple = true;
+        if (this.swUpdate.isEnabled) {
+            from(this.swUpdate.checkForUpdate()).subscribe((event) => {
+                if (event === true) {
+                    this.swUpdate.activateUpdate();
+                }
+            });
+        }
     }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,9 +18,8 @@ import { ServiceWorkerModule } from '@angular/service-worker';
         AdminLayoutModule,
         ToastModule,
         ServiceWorkerModule.register('ngsw-worker.js', {
-            enabled: !isDevMode(),
-            // Register the ServiceWorker as soon as the application is stable
-            // or after 30 seconds (whichever comes first).
+            enabled: true,
+
             registrationStrategy: 'registerWhenStable:30000',
         }),
     ],
