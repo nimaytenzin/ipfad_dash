f195eb3cc502a8f6ac3ab22a62ed13f0f3bcb2ee
nimaytenzin <nimaytenzin@gmail.com>
2024-07-02 12:26:58 +0600

    ok

--- src/app/presentations/tenant/profile/tenant-profile.component.ts
+++ src/app/presentations/tenant/profile/tenant-profile.component.ts
@@ -1,9 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { AvatarModule } from 'primeng/avatar';
+import { ButtonModule } from 'primeng/button';
+import { DividerModule } from 'primeng/divider';
 
 @Component({
     selector: 'app-tenant-profile',
     templateUrl: './tenant-profile.component.html',
     styleUrls: ['./tenant-profile.component.scss'],
+    standalone: true,
+    imports: [AvatarModule, ButtonModule, DividerModule],
 })
 export class TenantProfileComponent implements OnInit {
     constructor() {}
